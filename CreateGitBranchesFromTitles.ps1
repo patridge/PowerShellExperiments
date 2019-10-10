5cc0d877f1babce13190e3e972857b54c956c62b
Adam Patridge <patridge@gmail.com>
2019-10-10 12:00:23 -0600

    Add script from creating upstream feature branches

---
+++ CreateGitBranchesFromTitles.ps1
@@ -0,0 +1,6 @@
+# Assumes your upstream remote is "upstream"
+# Prefix is specific to Microsoft Learn, where we had a standard of "NEW-" for branches containing a new Learn module.
+@("This is some Title with capitalization", `
+  "Here is Another Cool module title") `
+| % { "NEW-" + ($_.Replace(" ", "-").Replace(",", "").ToLowerInvariant()) } `
+| % { git fetch upstream; git checkout -b $_ upstream/master; git push --set-upstream upstream $_ }
