8be27c72a5d581d167a4bde85902a70d14920935
Adam Patridge <patridge@gmail.com>
2019-10-25 13:39:26 -0600

    Command to kill off ADB processes

---
+++ NukeAdbInstances.ps1
@@ -0,0 +1,5 @@
+# If VS left an ADB instance around that prevents you from deleting or moving your Xamarin.Android projects, this will kill the process.
+# You might also be able to get away with `adb kill-server`.
+
+# This will find "adb" processes and stop them, then do a list of any it finds to verify you got them all.
+Get-Process | ? { $_.ProcessName -Eq "adb" } | % { Stop-Process -Id $_.Id }; Get-Process | ? { $_.ProcessName -Eq "adb" }
