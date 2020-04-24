c59ac109c3eec828e2582cc47c3fc5b5a0be1a37
Adam Patridge <patridge@gmail.com>
2020-04-24 09:59:37 -0600

    Add script to generate redirects for folder of modules

--- Learn-RedirectGeneration.ps1
+++ Learn-RedirectGeneration.ps1
@@ -3,6 +3,19 @@
 # 3. For each, generate a redirect JSON section
 Get-ChildItem *.yml | % { $_.Name.Replace(".yml", ".md") } | % { "{ ""source_path"": ""physical/repo/path/to/whatever-module-is-being-retired/$_"", ""redirect_url"": ""https://docs.microsoft.com/learn/new-module-replacing-the-retired-content"", ""redirect_document_id"": false }," }
 
+Get-ChildItem -Directory | % { Get-ChildItem $_ -Filter *.yml | % { Write-Output $_.Name } }
+
+ForEach ($moduleDirectory in Get-ChildItem -Directory) {
+    $unitYamlFiles = Get-ChildItem $moduleDirectory -Filter *.yml
+    $unitYamlFiles | % {
+        $ymlName = $_.Name
+        $mdName = $ymlName.Replace(".yml", ".md")
+        $noExtensionName = $_.Name.Replace(".yml", "")
+        $redirectLastSegment = $noExtensionName -eq "index" ? "" : "$noExtensionName/"
+        Write-Output "    {`n      ""source_path"": ""learn-pr/languages/$($moduleDirectory.Name)/$mdName"",`n      ""redirect_url"": ""https://docs.microsoft.com/learn/modules/$($moduleDirectory.Name)/$redirectLastSegment"",`n      ""redirect_document_id"": true`n    },"
+    }
+}
+
 # Notes
 # * The `source_path` is the local file path and may not look remotely similar to the web URL.
 # * Even though modules and paths don't have an index.md file locally, you still write the redirect against a `source_path` that includs `index.md`.
