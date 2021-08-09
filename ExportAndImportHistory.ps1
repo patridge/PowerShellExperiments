a3f75ed2d46f4dd6fe0f91c8d2836fe09ee4a2f7
Adam Patridge <patridge@gmail.com>
2021-08-09 14:14:59 -0600

    History export disclaimer

--- ExportAndImportHistory.ps1
+++ ExportAndImportHistory.ps1
@@ -1,3 +1,5 @@
+## NOTE: History and the lines you see when you press the up arrow are different. Exporting your history will not necessarily include everything you see when you press the up arrow.
+
 ## Export history to XML format
 Get-History | Export-Clixml -Path ~/Desktop/pwsh-history.xml
 
