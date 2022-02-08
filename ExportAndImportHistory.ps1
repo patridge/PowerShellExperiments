3c9dcf1b20230596720ec345fbbede0490758db2
Adam Patridge <patridge@gmail.com>
2022-02-08 13:18:08 -0700

    More history locations to view/search

--- ExportAndImportHistory.ps1
+++ ExportAndImportHistory.ps1
@@ -5,3 +5,7 @@ Get-History | Export-Clixml -Path ~/Desktop/pwsh-history.xml
 
 ## [untested] Import the history from exported XML
 Add-History -InputObject (Import-Clixml -Path ~\Desktop\pwsh-history.xml)
+
+## If you have PSReadline installed, there's a whole lot of console history to be found in log files it maintains.
+code "${env:APPDATA}\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
+code "${env:APPDATA}\Microsoft\Windows\PowerShell\PSReadline\Visual Studio Code Host_history.txt"
--- FindCommandFromFullPowerShellHistory.ps1
+++ FindCommandFromFullPowerShellHistory.ps1
@@ -1,5 +1,9 @@
 # This is to help find a prior command you know you ran based on some word within it.
 # Get-History is just for the current session, and not really that useful.
-# The contents of the file saved at `(Get-PSReadlineOption).HistorySavePath` is the real history.
+# If you have PSReadline installed, the contents of the file saved at `(Get-PSReadlineOption).HistorySavePath` is the real history.
 # In this case, I'm looking for a command I ran that included a call to `Remove-Item` somewhere within it.
 Get-Content (Get-PSReadlineOption).HistorySavePath | ? { $_ -like '*Remove-Item*' }
+
+# Also, there are also more comprehensive history locations that may be worth searching.
+Get-Content "${env:APPDATA}\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt" | ? { $_ -like '*Remove-Item*' }
+Get-Content "${env:APPDATA}\Microsoft\Windows\PowerShell\PSReadline\Visual Studio Code Host_history.txt" | ? { $_ -like '*Remove-Item*' }
