86fbaeebcd2adc9d5879e2ca99a4dff1027d60de
Adam Patridge <patridge@gmail.com>
2022-04-11 15:52:21 -0600

    Switch to Chocolatey-installed FFmpeg location

--- GoPro-VideoConcatenationAndProcessing.ps1
+++ GoPro-VideoConcatenationAndProcessing.ps1
@@ -2,7 +2,8 @@
 # Group all MP4 files of that GH*.MP4 format by the recording number (last four numbers) and filter by any grouping that contains more than one file, indicating it spans multiple files.
 
 # TODO: Edit for your own FFmpeg install location
-$ffmpegLocation = "~/Tools/ffmpeg/ffmpeg.exe"
+# $ffmpegLocation = "~/Tools/ffmpeg/ffmpeg.exe"
+$ffmpegLocation = "C:\ProgramData\chocolatey\bin\ffmpeg.exe"
 $outputCombinedVideoSuffix = "-combined"
 
 # Get all appropriate GoPro video segments without getting any previously generated combined video files (`-Exclude`).
@@ -31,18 +32,18 @@ $hockeyGameName = "${hockeyMyTeam} vs. ${hockeyGameOpponents}${videoLocation}${g
 $videoProjectName = "${hockeyGameDate} ${hockeyGameName}"
 
 Get-ChildItem | Sort-Object -Property LastWriteTime | ? { $_.Name.EndsWith(".MP4") } | % { $_.Name } | % { "file '" + $_ + "'" } > mylist.txt
-~/Tools/ffmpeg/ffmpeg.exe -f concat -safe 0 -i ./mylist.txt -c copy $videoProjectName-raw.mp4
+C:\ProgramData\chocolatey\bin\ffmpeg.exe -f concat -safe 0 -i ./mylist.txt -c copy $videoProjectName-raw.mp4
 #~/Tools/ffmpeg -f concat -safe 0 -i ./mylist.txt -c copy
 
 # Manual step: Find start and end times for next commands
 
-~/Tools/ffmpeg/ffmpeg.exe -i "./${videoProjectName}-raw.mp4" -ss 00:21:06 -to 01:11:38 -c copy "${videoProjectName}-trimmed.mp4"
+C:\ProgramData\chocolatey\bin\ffmpeg.exe -i "./${videoProjectName}-raw.mp4" -ss 01:06:28 -to 01:33:14 -c copy "${videoProjectName}-trimmed.mp4"
 
-~/Tools/ffmpeg -i "./${videoProjectName}-raw.mp4" -ss 00:31:50 -to 01:50:22 -c copy "${videoProjectName}-trimmed.mp4"
+#~/Tools/ffmpeg -i "./${videoProjectName}-raw.mp4" -ss 00:00:00 -to 00:56:00 -c copy "${videoProjectName}-trimmed.mp4"
 
 # Process for YouTube
 
-~/Tools/ffmpeg -i "./${videoProjectName}-trimmed.mp4" -vf yadif,format=yuv420p -force_key_frames "expr:gte(t,n_forced/2)" -c:v libx264 -crf 18 -bf 2 -c:a aac -q:a 1 -ac 2 -ar 48000 -use_editlist 0 -movflags +faststart "${videoProjectName}-processed.mp4"
+C:\ProgramData\chocolatey\bin\ffmpeg.exe -i "./${videoProjectName}-trimmed.mp4" -vf yadif,format=yuv420p -force_key_frames "expr:gte(t,n_forced/2)" -c:v libx264 -crf 18 -bf 2 -c:a aac -q:a 1 -ac 2 -ar 48000 -use_editlist 0 -movflags +faststart "${videoProjectName}-processed.mp4"
 
 Measure-Command { ~/Tools/ffmpeg/ffmpeg.exe -i "./${videoProjectName}-trimmed.mp4" -vf yadif,format=yuv420p -force_key_frames "expr:gte(t,n_forced/2)" -c:v libx264 -crf 18 -bf 2 -c:a aac -q:a 1 -ac 2 -ar 48000 -use_editlist 0 -movflags +faststart "${videoProjectName}-processed.mp4" }
 
