f08864ba9724d8b920f3df2c936037b2750636df
Adam Patridge <patridge@gmail.com>
2022-07-12 15:26:34 -0600

    Initial bulk repo status script

---
+++ GitCliScripts.ps1
@@ -0,0 +1,12 @@
+# List the branch and state/status from upstream
+$upstreamRemote = "upstream"; `
+Get-ChildItem -Directory `
+  | Where-Object Name -NotIn @("_myprojects", "NetduinoSamples") `
+  | ForEach {  `
+    Set-Location $_; `
+    $currentBranch = (git branch --show-current); `
+    $commitsAhead = (git rev-list $currentBranch --not $upstreamRemote/$currentBranch --count); `
+    $commitsBehind = (git rev-list $upstreamRemote/$currentBranch --not $currentBranch --count); `
+    Write-Host "$($_.Name): $(git branch --show-current) ↓$commitsAhead ↑$commitsBehind"; `
+    Set-Location .. `
+  }
