ac46ad6d218cf2b54d3a47a3f5ea7b402a672b23
Adam Patridge <patridge@gmail.com>
2022-07-12 18:20:01 -0600

    Still not working yet within the ForEach loop

--- GitCliScripts.ps1
+++ GitCliScripts.ps1
@@ -10,3 +10,15 @@ Get-ChildItem -Directory `
     Write-Host "$($_.Name): $(git branch --show-current) ↓$commitsAhead ↑$commitsBehind"; `
     Set-Location .. `
   }
+
+$upstreamRemote = "upstream"; `
+Get-ChildItem -Directory `
+  | Where-Object Name -NotIn @("_myprojects", "NetduinoSamples") `
+  | ForEach { `
+    Set-Location $_; `
+    $currentBranch = (git branch --show-current); `
+    $commitsAhead = (git rev-list $currentBranch --not $upstreamRemote/$currentBranch --count); `
+    $commitsBehind = (git rev-list $upstreamRemote/$currentBranch --not $currentBranch --count); `
+    # Write-Host "$($_.Name): $(git branch --show-current) ↓$commitsAhead ↑$commitsBehind"; `
+    Set-Location .. `
+  }
