452250c95525dbcfc458cf5f01c975902b98b168
Adam Patridge <patridge@gmail.com>
2022-07-21 07:40:24 -0600

    Bulk Learn Git branch updates

--- LearnPrUpdateFeatureBranches.ps1
+++ LearnPrUpdateFeatureBranches.ps1
@@ -7,9 +7,9 @@
   "branch-name-6", `
   "branch-name-7",) `
 | Select -First 1 ` # This line limits to first branch for testing. Remove this line to operate across all branch names.
-| % { git fetch upstream; git checkout -b $_ upstream\$_; git merge upstream/master; }
+| % { git fetch upstream; git checkout -b $_ upstream\$_; git merge upstream/main; }
 
 # Update a remote feature branch by PR with the latest from remote `master`.
 # Given a remote of `upstream`...fill out `{some-org}`, `{some-repo}`, `{some-fork}`, `{some-body}`...
 # Given a string representing a Git branch name, merge latest master from upstream, open a Chrome window for the pending PR, and copy the command to push to fork branch to the clipboard. If the merge failed, it wouldn't be used and I would `git merge --abort` instead. Otherwise, I would push and start a PR at the opened Chrome window.
-@("branch-name-1") | % { git fetch upstream; git checkout -b $_ upstream/$_; git merge upstream/master; Start-Process "chrome.exe" "https://github.com/{some-org}/{some-repo}/compare/$_...{some-fork}:$_?expand=1&title=Update%20with%20the%20latest%20master&body={some-body}" }; "git push --set-upstream {some-fork} $_" | clip;
+@("branch-name-1") | % { git fetch upstream; git checkout -b $_ upstream/$_; git merge upstream/main; Start-Process "chrome.exe" "https://github.com/{some-org}/{some-repo}/compare/$_...{some-fork}:$_?expand=1&title=Update%20with%20the%20latest%20master&body={some-body}" }; "git push --set-upstream {some-fork} $_" | clip;
