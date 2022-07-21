4d8c80e9e7d472aa56ab4e2a6ca5c92b54f0a1f0
Adam Patridge <patridge@gmail.com>
2022-07-21 07:46:30 -0600

    Add Learn-specific notes

--- Git-CreateBranchesFromLearnTitles.ps1
+++ Git-CreateBranchesFromLearnTitles.ps1
@@ -1,5 +1,6 @@
-# Assumes your upstream remote is "upstream"
+# This was a system specific to creating Git branch names from natural Learn module names.
 # Prefix is specific to Microsoft Learn, where we had a standard of "NEW-" for branches containing a new Learn module.
+# Assumes your upstream remote is "upstream" and that development is done from a branch named "main".
 @("This is some Title with capitalization", `
   "Here is Another Cool module title") `
 | % { "NEW-" + ($_.Replace(" ", "-").Replace(",", "").ToLowerInvariant()) } `
