20ef23551acbb173f8f7c970129a056daa45687e
Adam Patridge <patridge@gmail.com>
2023-04-23 06:07:15 -0700

    Update UserProfileTips.ps1

--- UserProfileTips.ps1
+++ UserProfileTips.ps1
@@ -3,4 +3,4 @@
 code $PROFILE
 ## Add this shortcut to the profile
 ### Via Nick Craver (https://twitter.com/nick_craver/status/1584407350235910144)
-function New-Tab { wt --window 0 new-tab --directory . }
+function New-Tab { wt --window 0 new-tab --startingDirectory . }
